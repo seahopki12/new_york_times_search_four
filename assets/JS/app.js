01ebd5cd3828a403d3ea2d69dc00433e4f437c71
Chef Brams <oldcitycatering@gmail.com>
2020-09-17 15:12:57 -0400

    variables

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,9 +1,15 @@
 
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=";
-var key = "4d4GyNsASxu5AAQKfKGSS0A9gOvDyxbq";
+var apiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=";
+var apikey = "4d4GyNsASxu5AAQKfKGSS0A9gOvDyxbq";
+var searchTerm = $("#term").val();
+var recordNUM = $("#records").val();
+var startYear = $("#startY").val();
+var endYear = $("#endY").val();
+var queryURL = apiURL + apikey;
 $.ajax({
-    url: queryURL + key,
+
+    url: queryURL,
     method: "GET"
 }).then(function (response) {
     console.log(response);
