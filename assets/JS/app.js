1ff248d63a58ccca8840edd36de03f9ced3eaae8
Chef Brams <oldcitycatering@gmail.com>
2020-09-18 14:40:42 -0400

    updated js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,16 +1,28 @@
 
 
-var apiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=";
-var apikey = "4d4GyNsASxu5AAQKfKGSS0A9gOvDyxbq";
-var searchTerm = $("#term").val();
-var recordNUM = $("#records").val();
+
+var apiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
+var apikey = "&api-key=4d4GyNsASxu5AAQKfKGSS0A9gOvDyxbq";
+var searchTerm = "GWU&begin_date=2019&end_date=2020";
+var recordNUM = $("#records").val() || 5;
 var startYear = $("#startY").val();
 var endYear = $("#endY").val();
-var queryURL = apiURL + apikey;
+var queryURL = apiURL + toString(searchTerm) + apikey;
+
+
+
+
 $.ajax({
 
     url: queryURL,
     method: "GET"
 }).then(function (response) {
     console.log(response);
+    for (var i = 0; i < parseInt(recordNUM); i++) {
+        var headline = response.response.docs[i].headline.print_headline
+        var artURL = response.response.docs[i].web_url
+        // var link = $("<a>").attr("")
+        $("#articles").append(`<li> <a href=${artURL} target="_blank"><h1>${headline}</h1></a> </li>`);
+    }
+
 });
